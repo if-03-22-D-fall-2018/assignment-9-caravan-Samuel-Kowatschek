b624a32a9738073cb45559403f1ffa6e9fb2ea96
admin <admin@localhost.localdomain>
2018-12-21 23:10:21 +0100

    started new_caravan and get_length, created an array and a struct

--- caravan.cpp
+++ caravan.cpp
@@ -10,12 +10,35 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include "caravan.h"
+
+struct CaravanImplementation{
+    int length;
+};
+
+struct CaravanImplementation c1;
+struct CaravanImplementation c2;
+struct CaravanImplementation c3;
+struct CaravanImplementation c4;
+struct CaravanImplementation c5;
+
+static Caravan caravans[] = {&c1, &c2, &c3, &c4, &c5};
+
+
 Caravan new_caravan()
 {
+    
+    for(int i = 0; i < 5; i++)
+    {
+        if(caravans[i]->length==0){
+            return caravans[i];
+        }
+    }
 }
 
 int get_length(Caravan caravan)
 {
+    return caravan->length;
 }
 
 void delete_caravan(Caravan caravan)
